0318cd57c0cf25af5954d47bc5530c23db227261
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-07-29 10:42:39 +0900

    implement pbr.

---
+++ CrystalPBR/CrystalPBR/SkyBoxRenderer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Crystal/Shader/IRenderer.h"
+
+#include "Crystal/Shader/CubeMapTextureObject.h"
+#include <string>
+
+namespace Crystal {
+	namespace Shader {
+
+class SkyBoxRenderer : public IRenderer
+{
+public:
+	struct Buffer
+	{
+		Math::Matrix4dd projectionMatrix;
+		Math::Matrix4dd modelViewMatrix;
+		CubeMapTextureObject* cubeMapTexture;
+	};
+
+	SkyBoxRenderer();
+
+	ShaderBuildStatus build(Shader::GLObjectFactory& factory) override;
+
+	void release(Shader::GLObjectFactory& factory) override;
+
+	void render(const Buffer& buffer);
+
+	std::string getName() const override { return "SkyBoxRenderer"; }
+
+private:
+	std::string getBuiltInVertexShaderSource() const;
+
+	std::string getBuiltInFragmentShaderSource() const;
+
+	Shader::ShaderObject* shader;
+};
+
+	}
+}
