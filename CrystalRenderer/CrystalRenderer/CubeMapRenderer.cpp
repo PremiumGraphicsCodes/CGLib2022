fb4a53215f9217919a6cc8376bfa2dde4e1fd279
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-08-10 14:28:29 +0900

    fix bug.

--- CrystalRenderer/CrystalRenderer/CubeMapRenderer.cpp
+++ CrystalRenderer/CrystalRenderer/CubeMapRenderer.cpp
@@ -1,7 +1,11 @@
 #include "CubeMapRenderer.h"
 
 #include "Crystal/Shader/TextureObject.h"
+#include "Crystal/Math/Box3d.h"
 
+#include "Crystal/Shape/TriangleMeshBuilder.h"
+
+using namespace Crystal::Math;
 using namespace Crystal::Shader;
 using namespace Crystal::Renderer;
 
@@ -29,50 +33,27 @@ void CubeMapRenderer::link()
 namespace {
 	std::vector<float> getCubePositions()
 	{
-		return {
-            // back face
-            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
-             1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
-             1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
-             1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
-            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
-            -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
-            // front face
-            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
-             1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
-             1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
-             1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
-            -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
-            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
-            // left face
-            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
-            -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
-            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
-            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
-            -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
-            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
-            // right face
-             1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
-             1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
-             1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
-             1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
-             1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
-             1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
-            // bottom face
-            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
-             1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
-             1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
-             1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
-            -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
-            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
-            // top face
-            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
-             1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
-             1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
-             1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
-            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
-            -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
-        };
+        Crystal::Shape::TriangleMeshBuilder builder;
+        Box3dd box(Vector3dd(-1, -1, -1), Vector3dd(1, 1, 1));
+        builder.add(box, 2, 2, 2);
+        const auto tm = builder.build();
+        const auto fs = tm->getFaces();
+        std::vector<float> positions;
+        for (auto t : fs) {
+            const auto v0 = t.triangle.getV0();
+            const auto v1 = t.triangle.getV1();
+            const auto v2 = t.triangle.getV2();
+            positions.push_back(v0.x);
+            positions.push_back(v0.y);
+            positions.push_back(v0.z);
+            positions.push_back(v1.x);
+            positions.push_back(v1.y);
+            positions.push_back(v1.z);
+            positions.push_back(v2.x);
+            positions.push_back(v2.y);
+            positions.push_back(v2.z);
+        }
+        return positions;
 	}
 }
 
@@ -92,7 +73,7 @@ void CubeMapRenderer::render()
 	shader->sendVertexAttribute3df(::positionLabel, positions);
 
 	glEnableVertexAttribArray(0);
-	shader->drawTriangles(positions.size() / 3);
+    shader->drawTriangles(positions.size() / 3);
 	glDisableVertexAttribArray(0);
 
 	buffer.texture->unbind();
